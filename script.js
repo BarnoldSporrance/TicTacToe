282c6f3288e1d8120487432096cc17c2046941bc
Barry Lawrence <barnoldsporrance@gmail.com>
2021-03-16 16:55:04 +0300

    grid stylized

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const gameboard =(function() {
-  const gameArray =["1","2","3","4","5","6","7","8","9"];
+  const gameArray =["X","O","X","X","X","O","X","O","X"];
   
   for (i=0; i<=8;i++){
     let cell = document.getElementById(i);
     cell.innerText = gameArray[i];
   }
+})();
+
+const players = (function() {
+const playerOne = prompt("Enter the name for player one");
+const playerTwo = prompt("Enter the name for player two");
+return {
+   playerOne, playerTwo
+};
 
 })();
