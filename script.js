86054afc142158354a79472fbacef960e6a458f8
Barry Lawrence <barnoldsporrance@gmail.com>
2021-04-11 20:40:47 +0300

    Two player game bascially intact.

--- script.js
+++ script.js
@@ -13,9 +13,13 @@ const model = {
   const logicVar = model.gameArrayFunction.gameArray;
 
   if (logicVar[0]=== "x" && logicVar[1] === "x" && logicVar[2] === "x" || logicVar[3]=== "x" && logicVar[4] === "x" && logicVar[5] === "x" || logicVar[6]=== "x" && logicVar[7] === "x" && logicVar[8] === "x" || logicVar[0]=== "x" && logicVar[4] === "x" && logicVar[8] === "x" || logicVar[2]=== "x" && logicVar[4] === "x" && logicVar[6] === "x" || logicVar[0]=== "x" && logicVar[3] === "x" && logicVar[6] === "x"|| logicVar[1]=== "x" && logicVar[4] === "x" && logicVar[7] === "x" || logicVar[2]=== "x" && logicVar[5] === "x" && logicVar[8] === "x"){
-    winner = controller.players.playerOne;
+    controller.getEntry.playerSelector = "x";
+    winner = controller.getEntry.playerSelector;
+    
   } else if (logicVar[0]=== "o" && logicVar[1] === "o" && logicVar[2] === "o" || logicVar[3]=== "o" && logicVar[4] === "o" && logicVar[5] === "o" || logicVar[6]=== "o" && logicVar[7] === "o" && logicVar[8] === "o" || logicVar[0]=== "o" && logicVar[4] === "o" && logicVar[8] === "o" || logicVar[2]=== "o" && logicVar[4] === "o" && logicVar[6] === "o" || logicVar[0]=== "o" && logicVar[3] === "o" && logicVar[6] === "o"|| logicVar[1]=== "o" && logicVar[4] === "o" && logicVar[7] === "o" || logicVar[2]=== "o" && logicVar[5] === "o" && logicVar[8] === "o"){
-    winner = controller.players.playerTwo;
+    controller.getEntry.playerSelector = "o";
+    winner = controller.getEntry.playerSelector;
+    
   }
  return {winner}
   },// end game logic
@@ -24,54 +28,48 @@ const model = {
 
 const controller = {
 
-  //factory function to create players
-  players: (function(){
-    
-    const playerOne = prompt("Player one's name?");
+  getGameMode: (function(){
+    const playerOneButton =  document.getElementById("playerOneButton");
+    const playerTwoButton = document.getElementById("PlayerTwoButton");
 
-    const playerTwo = prompt("Player two's name?");
-    
-    return {
-     playerOne, playerTwo
-  };
-  })(), // end player factory function
+    playerOneButton.addEventListener('click', ()=> {
+       
+    }) // end event listener click
+  }),
 
   getEntry: (function(){
     //get input from player and send to model array
-    const cells = document.getElementsByTagName("td");
+    const cells = document.querySelectorAll(".cell");
+
+    let playerSelector = "x";
     for (var i=0; i<=8; i++){
-      cells[i].addEventListener('focusout', function(event) {
+      cells[i].addEventListener('click', function(event) {
+       
       let cellID = event.target.id;
-      let cellContent = this.innerText;
-      
-     // function to check that entry is either an X or O
-     const checkInput = (function(){
-      if (cellContent === "x"|| cellContent === "X" || cellContent === "o" || cellContent === "O" || cellContent === " " || cellContent ==="") {
-       // console.log("valid input");
-        cellContent = cellContent.toLowerCase();
-        // send valid inout to model
-        model.gameArrayFunction.gameArray.splice(cellID, 1,cellContent);
-        view.displayBoard();
-        model.gameLogic();
-        
+      if (playerSelector === "x") {
+        console.log("playerSelector: " + playerSelector);
+      model.gameArrayFunction.gameArray.splice(cellID, 1, playerSelector);
+      playerSelector ="o";
+      } else if (playerSelector ==="o") {
+        console.log("playerSelector: " + playerSelector);
+        model.gameArrayFunction.gameArray.splice(cellID, 1, playerSelector);
+        playerSelector ="x";
+      }
+      view.displayBoard();
+
         if (model.gameLogic().winner !=="none"){
+         
         view.displayWinner(model.gameLogic().winner);
         }
-      } else {
-      //  console.log("pure shite input");
-        cellContent = ' ';
-        view.displayBoard();
-        model.gameLogic();
-        alert("hey! just the 'x's and 'o's, please.");
-      }
-     })(); // end check input
+     
       }); // end event listener
     } // end for 
+  return{ playerSelector}
+
   })() //end get entry function
 } // end controller
 
 const view = {
-
   // populate the board with currently positioned Xs and Os
   displayBoard: (function(){
   for (i=0; i<=8;i++){
@@ -80,18 +78,34 @@ const view = {
     }
   }), // end populate the on-page grid loop
 
-  //dipslay the player names on the board
-  displayPlayerNames:(function(){
-  const displayPlayerOne = document.getElementById("playerOne");
-  displayPlayerOne.innerText = "Player 1(x): " + controller.players.playerOne;
+  mouseOverEffect:(function(){
+    const focusedSquare = document.querySelectorAll('.cell');
+    focusedSquare.forEach((div) =>{
+      div.addEventListener('mouseover',()=> {
+       
+        div.classList.add('mousedOver');
+      }); // end event listener 'mouseover'
+    }); // end 'for each' 
+  })(), // end mouseOverEffect
 
-  const displayPlayerTwo = document.getElementById("playerTwo");
-  displayPlayerTwo.innerText = "Player 2(o): " + controller.players.playerTwo;
-  }),
+  mouseOutEffect:(function(){
+    const unfocusedSquare = document.querySelectorAll('.cell');
+    unfocusedSquare.forEach((div) =>{
+      div.addEventListener('mouseout',()=> {
+        div.classList.remove('mousedOver')
+   
+      }); // end event listener 'mouseout'
+    }); // end 'for each' 
+  })(), // end mouseOutEffect
 
   displayWinner: (function(winner){
+  //  view.displayBoard();
   alert("And the winner is " + winner);
+ // model.gameArrayFunction.gameArray = [" "," "," "," "," "," "," "," "," "];
+  view.displayBoard();
   })
 } // end view object
 
-view.displayPlayerNames();
+view.displayBoard();
+
+
